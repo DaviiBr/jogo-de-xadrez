bb8eff3a8002c037365c39629c0d0ba418c379ff
Davi Brasil <davibrclau@gmail.com>
2023-12-11 21:36:35 -0300

    first class: position

---
+++ src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+
+
+
+        }
+    }
